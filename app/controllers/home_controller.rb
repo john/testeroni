664220199265983a3b94d90a14e3199f36765fc6
john <john@wordie.org>
2011-02-05 21:59:29 -0800

    fixed sort order of tests on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,14 @@ class HomeController < ApplicationController
     @title = "Testeroni - Make and take tests and surveys"
     @description = "Testeroni lets you make surveys and quizzes from YouTube videos."
     # @tests = Tst.recently_published
+    
+    # the join with questions is to insure empty tests aren't displayed
     @tests = Tst.find_by_sql(["
       SELECT DISTINCT t.* from tsts t, questions q
       WHERE q.tst_id = t.id
       AND t.published_at IS NOT NULL
-      AND t.published_at <= ? ", Time.zone.now])
+      AND t.published_at <= ?
+      ORDER BY t.published_at DESC", Time.zone.now])
   end
   
   def notes
