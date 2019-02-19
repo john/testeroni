089f732cbd925f9ff165bd040ccc276319f9c477
John McGrath <john@fryolator.com>
2019-02-19 08:47:57 -0800

    add specs skeleton

---
+++ spec/models/tst_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Tst, type: :model do
+  # describe 'public lists' do
+  #   it 'should return public lists' do
+  #     create(:public_list)
+  #     public_lists = List.is_public
+  #     expect(public_lists.length).to eq(1)
+  #   end
+  #
+  #   it 'should have correct status' do
+  #     l = create(:public_list)
+  #     expect(l.is_public?).to eq( true )
+  #     expect(l.is_private?).to eq( false )
+  #   end
+  # end
+  #
+  # describe 'private lists' do
+  #   it 'should have correct status' do
+  #     l = create(:private_list)
+  #     expect(l.is_public?).to eq( false )
+  #     expect(l.is_private?).to eq( true )
+  #   end
+  # end
+end
